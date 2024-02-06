ed5e3881b3debafa4bab7280c7dd081eeda50f04
rmurtn <rmurtn@gmail.com>
2024-02-06 14:28:28 -0800

    added getMessage() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello git");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello git";
     }
 }
