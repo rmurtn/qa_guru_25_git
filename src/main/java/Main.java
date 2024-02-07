44281e0a15c80a1aa377a4548eeaf54f0a43c93f
rmurtn <rmurtn@gmail.com>
2024-02-07 10:44:07 -0800

    added msg2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,13 @@ public class Main {
 
     private static String msg="Hello git";
 
+    private static String msg2="Let's get started";
+
     public static void main(String[] args) {
         System.out.println(getMessage());
     }
 
     private static String getMessage() {
-        return msg;
+        return msg + msg2;
     }
 }
