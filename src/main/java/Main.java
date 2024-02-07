4c8baa860e4dc506a948a25eed6bc78950bf341d
rmurtn <159101465+rmurtn@users.noreply.github.com>
2024-02-07 12:08:46 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     private static String msg="Hello git";
 
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println(getMessage);
     }
 
     private static String getMessage() {
